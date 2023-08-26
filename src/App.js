f6740a26b78321af3274a521aaf3550980a192c5
TheNam74 <nguyenthenam1910@gmail.com>
2023-08-26 09:43:36 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Popconfirm, Button } from "antd";
+
+function App() {
+    return (
+        <div className="App">
+            <Popconfirm
+                title="Delete the task"
+                description="Are you sure to delete this task?"
+                okText="Yes"
+                cancelText="No"
+            >
+                <Button danger>Delete</Button>
+            </Popconfirm>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
